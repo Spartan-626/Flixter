83e8e01a26c92a059501f2f42f7aff666d13d022
Spartan-626 <kwilliamson0232@gmail.com>
2019-10-14 23:38:09 +0000

    Problem with the routes file. Saying No route matches [GET] /instructor/courses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :models
   root 'static_pages#index'
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
