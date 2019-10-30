340ae449502a1661eb64965b9b18fb11dccdf632
Spartan-626 <kwilliamson0232@gmail.com>
2019-10-30 02:29:21 +0000

    Changed carrierwave.rb file back to original state without personal access keys...oops.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     ENV["AKIAVXVV7TSWVPH73SQO"],        # required
-      aws_secret_access_key: ENV["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"],        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
     }
-    config.fog_directory  = ENV["kyles-flixter-bucket"]              # required
+    config.fog_directory  = ENV["AWS_BUCKET"]              # required
   end
