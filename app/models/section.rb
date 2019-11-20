de33721cfcc8b4666c5c68cc0a764e6ff019b4cd
Spartan-626 <kwilliamson0232@gmail.com>
2019-11-20 03:51:58 +0000

    Started to add reordering of sections.

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,6 +2,8 @@ class Section < ApplicationRecord
     belongs_to :course
     has_many :lessons
 
+    include RankedModel
+    ranks :row_order, with_same: :course_id
     validates :title, presence: true
     #validates :description, presence: true
     #validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
